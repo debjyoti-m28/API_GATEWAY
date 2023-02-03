b01f973107f2be3007fe326fc319cb2a117ee604
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2023-02-03 23:21:43 +0530

    morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
+
+app.use(morgan('combined'));
 
 const startServer = () => {
     app.listen(PORT, (req, res) => {
